dc3182fb03a2f02b16d402c2fed9efabc98dad8f
B1nj0y <idegorepl@gmail.com>
2021-03-16 09:42:33 +0800

    Create get_versions.sh
    Add a bash script to get versions

---
+++ get_versions.sh
@@ -0,0 +1,5 @@
+# GET /README.md
+
+echo "Ruby Docker image major versions"
+
+wget -q https://registry.hub.docker.com/v1/repositories/ruby/tags -O - | jq ".[].name" | perl -nle 'print "* $1" if /^"(\d+\.[\d+\.?]+)"/'
